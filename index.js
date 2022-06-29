eb00952131b0f3e3a7db7dd63287f0c4088a0dd6
Andrianna <andrianna.odell@gmail.com>
2022-06-28 22:17:23 -0400

    commit tree d3f30f92bf4cedd5e9f6fce1b44fa16ed130930b

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code your solution here
+
+// create default function that can have new argument placed inside
+function saturdayFun(activity = "roller-skate"){
+return `This Saturday, I want to ${activity}!`;
+}
+
+// create function expression that has default result that can be changed
+const mondayWork = (function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+});
+
+// define wrapAdjective
+function wrapAdjective(flare){
+    const result = function(){
+        let emphatic;
+        let result;
+        if(flare === "*"){
+            result = "a hard worker";
+            emphatic = `You are ${flare}${result}${flare}!`
+        }
+        else if(flare === "||"){
+            result = "a dedicated programmer";
+            emphatic = `You are ${flare}${result}${flare}!`
+        }
+        return emphatic;
+    }
+    return result;
+}
+
+// result will get the value from the return of wrapAdjective
+// return of wrapAdjective will be added to the building of
+// the sentence
+
+
+// emphatic will get the value of result
+// and concatenate to create a sentence
+
